9aab453b8a57b744a2bb6d337d245fd7eae78d6a
Mark Su <marksu@Marks-MBP13-2020.local>
2023-05-23 23:46:40 +0800

    create sdk for mumbai

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  MUMBAI = 80001
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x4231c60cCD5bA04d60312E7Ac7C3835B9e6a44a9'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x55b09bee0c1316750f7a82f3cf4dfe6806b1511e3eb8827b7428d6cdc3d24cd3'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
